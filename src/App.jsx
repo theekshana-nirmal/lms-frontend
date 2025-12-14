05ce826b960a85399d0aa82932d15298c4b57bdf
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-14 11:55:45 +0530

    feat: replace placeholder text with Hero component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 className="text-2x text-amber-300">Hello</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, a.</p>
+      <Hero />
     </>
   );
 }
