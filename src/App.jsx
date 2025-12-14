ca8c21bd831cccb5645b2ef7c32e2053318a5979
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-14 11:06:07 +0530

    feat: add Navbar component to App for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="text-2x text-amber-300">Hello</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, a.</p>
     </>
