bf1fc23996cb2fee53e8856d7ca10c2e56cf8613
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-14 11:04:53 +0530

    feat: implement Navbar component with navigation links and buttons

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import { NAV_LINKS } from "@/constants/data";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="max-w-full px-2 sm:px-8 lg:px-16 py-4 flex items-center justify-between border-b border-gray-200">
+      <div className="font-bold text-xl text-gray-800">LMS</div>
+
+      <div className="flex gap-8">
+        {NAV_LINKS.map((link) => (
+          <Link key={link.name} to={link.href}>
+            {link.name}
+          </Link>
+        ))}
+      </div>
+
+      <div className="flex gap-4">
+        <Button variant="outline" asChild>
+          <Link to="/login">Login</Link>
+        </Button>
+        <Button asChild>
+          <Link to="/register">Register</Link>
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
