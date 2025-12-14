5f68e39c0d41205a7f96d121292112329720f910
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-14 23:32:56 +0530

    feat: Add lazy loading to images in FAQ and Features components for improved performance

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -8,11 +8,15 @@ import {
 
 const FAQ = () => {
   return (
-    <div id="faq" className="flex flex-col md:flex-row items-center max-w-7xl mx-auto gap-8 py-8 md:py-16 px-4 sm:px-8 border-t border-gray-200">
+    <div
+      id="faq"
+      className="flex flex-col md:flex-row items-center max-w-7xl mx-auto gap-8 py-8 md:py-16 px-4 sm:px-8 border-t border-gray-200"
+    >
       <div className="flex flex-col flex-1 order-2 md:order-1">
         <img
           src={FAQ_CONTENT.image}
           alt="FAQ image"
+          loading="lazy"
           className="w-full max-w-160 border border-gray-200 rounded-2xl"
         />
       </div>
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,7 +2,10 @@ import { FEATURES_CONTENT } from "@/constants/data";
 
 const Features = () => {
   return (
-    <div id="features" className="flex flex-col md:flex-row max-w-7xl mx-auto gap-8 py-8 md:py-16 px-4 sm:px-8">
+    <div
+      id="features"
+      className="flex flex-col md:flex-row max-w-7xl mx-auto gap-8 py-8 md:py-16 px-4 sm:px-8"
+    >
       <div className="flex flex-col flex-1 gap-8 p-4 sm:p-8 md:p-16">
         <h2 className="text-2xl sm:text-3xl md:text-4xl">
           {FEATURES_CONTENT.title}
@@ -19,6 +22,7 @@ const Features = () => {
         <img
           src={FEATURES_CONTENT.image}
           alt="Features"
+          loading="lazy"
           className="w-full max-w-160 border border-gray-200 rounded-2xl"
         />
       </div>
