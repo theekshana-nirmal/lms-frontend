252799e409a6785d68a9cb984afdde808fa240e4
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-14 23:24:19 +0530

    feat: Refactor routing setup and add NotFoundPage with 404 image

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <LandingPage /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "*", element: <NotFoundPage /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
