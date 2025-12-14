cfbc81d9a68305154607d05f132efb7a131a0cd8
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-14 13:13:28 +0530

    feat: add Features component to App and enhance typography styles in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/layout/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Features />
     </>
   );
 }
