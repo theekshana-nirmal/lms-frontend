f7cc0cfd1aef39c40a917bf2a5d99194e7ca4016
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-15 15:59:12 +0530

    feat: Create Dashboard component with Navbar and Footer

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <div>User Dashboard</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
