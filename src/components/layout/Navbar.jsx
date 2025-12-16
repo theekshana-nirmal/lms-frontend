c6131052e4d9e404672147fd971fcedfdcec5ca6
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-16 15:02:24 +0530

    feat: implement login/logout functionality in Navbar component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,8 +1,20 @@
 import { NAV_LINKS } from "@/features/landing/constants/landingContent";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] =  useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    } 
+  }, []);
+
   return (
     <nav className="max-w-full px-2 sm:px-8 lg:px-16 py-4 flex items-center justify-between border-b border-gray-200 fixed top-0 bg-white w-full z-10">
       <div className="font-bold text-xl text-gray-800">
@@ -22,12 +34,22 @@ const Navbar = () => {
       </div>
 
       <div className="flex gap-2 sm:gap-4">
-        <Button variant="outline" asChild size="sm">
-          <Link to="/login">Login</Link>
-        </Button>
-        <Button asChild size="sm">
-          <Link to="/register">Register</Link>
-        </Button>
+        {loggedIn ? (
+            <Button variant="outline" onClick={() => {
+              localStorage.clear();
+              setLoggedIn(false);
+              navigate("/login", { replace: true });
+            }}>Logout</Button>
+        ) : (
+          <>
+            <Link to="/login">
+              <Button variant="outline">Login</Button>
+            </Link>
+            <Link to="/register">
+              <Button>Sign Up</Button>
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
