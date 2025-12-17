0beb160bc31335d69ff5c257b94da42a690dd9e1
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-17 12:38:31 +0530

    feat: simplify ProtectedRoute component structure and formatting

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -1,18 +1,11 @@
-import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "@/utils/storage.js";
-import PropTypes from "prop-types";
+import {Navigate} from "react-router-dom";
+import {isAuthenticated} from "@/utils/storage.js";
 
-const ProtectedRoute = ({ children }) => {
-  if (!isAuthenticated()) {
-    return <Navigate to="/login" replace />;
-  }
-  return children;
+const ProtectedRoute = ({children}) => {
+    if (!isAuthenticated()) {
+        return <Navigate to="/login" replace/>;
+    }
+    return children;
 };
-
-ProtectedRoute.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-ProtectedRoute.displayName = 'ProtectedRoute';
 
 export default ProtectedRoute;
